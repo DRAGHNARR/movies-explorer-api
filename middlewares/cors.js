05eec9ce37f38c1bcb482fce73451672446bead7
Denis Kochnev <draghnarrok@gmail.ru>
2021-07-16 03:27:49 +0300

    1-st

---
+++ middlewares/cors.js
@@ -0,0 +1,19 @@
+const allowedCors = [
+  'http://ourtube.nomoredomains.work',
+  'https://ourtube.nomoredomains.work',
+  'http://localhost:3000',
+  'https://localhost:3000',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  console.log(origin);
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  }
+
+  next();
+};
